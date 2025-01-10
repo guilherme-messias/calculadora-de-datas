d49bdbcf08a20e0417c6cfb56c2c2081cd72b6ae
Guilherme Messias <messiasguilherme700@gmail.com>
2025-01-10 14:49:04 -0300

    Corrige importação do módulo de cálculo de dias entre datas para incluir a extensão .js

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import calculateDaysBetweenDates from "./calculateDaysBetweenDates";
+import calculateDaysBetweenDates from "./calculateDaysBetweenDates.js";
 document.getElementById("btn-submit").addEventListener("click", () => {
     const startDate = document.getElementById("startDate").value;
     const endDate = document.getElementById("endDate").value;
